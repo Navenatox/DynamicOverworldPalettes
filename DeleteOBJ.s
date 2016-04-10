bfe5272afc3d543622ff0dc6ef9aac22ac5e4c5f
Navenatox <navenatox@hotmail.com>
2016-04-10 15:18:58 +0200

    Added fix for the Move Deleter menu

--- DeleteOBJ.s
+++ DeleteOBJ.s
@@ -1,7 +1,7 @@
 .thumb
 
 
-DeleteOBJ: @ hook at 0x7280
+DeleteOBJFreeTiles: @ hook at 0x7280
 	push {r4-r7,lr}
 	mov r5, r0
 	ldrb r0, [r5,#5]
@@ -12,6 +12,13 @@ DeleteOBJ: @ hook at 0x7280
 	ldrb r1, [r0]
 	ldr r0, =0x8007288+1
 	bx r0
+
+
+DeleteAllOBJs: @ hook at 0x779C
+	ldr r1, =0x80073DC+1 @ DeleteOBJ
+	bl CallByR1
+	bl ClearAllPalRefs
+	pop {r4,r5,pc}
 
 
 DeleteReflection: @ hook at 0xDAF88
@@ -50,5 +57,9 @@ DeleteWarpArrow: @ hook at 0xDB120
 	orr r0, r1
 	strb r0, [r2]
 	pop {pc}
+
+
+CallByR1:
+	bx r1
 
 
